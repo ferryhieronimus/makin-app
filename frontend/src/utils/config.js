aa662aa6f39ac32fe3820f181a09b8170a4dd845
Hieronimus Ferry <ferryhieronimus@gmail.com>
2023-08-30 03:26:37 +0700

    fix: change url in config

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   local: "http://localhost:3000",
-  prod: "https://api.makin.hieronimusferry.com"
+  prod: "https://api-makin.hieronimusferry.com"
 };
 
 export default config[process.env.REACT_APP_NODE_ENV || "local"];
